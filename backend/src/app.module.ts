c15cce4c3aba49e07e53a852b9718ec9e54c1a22
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-12-06 22:12:32 -0300

    feat: it removes DigimonsModule

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { DigimonsModule } from './digimons/digimons.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), DigimonsModule],
   controllers: [AppController],
   providers: [AppService],
 })
