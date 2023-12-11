6bf17b3706695fa656cbc8444af3281122693e75
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-12-11 20:28:57 -0300

    feat: add notfound component

---
+++ frontend/src/app/components/notFound.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="mt-[160px] flex flex-row items-center justify-center px-2">
+      <h1 className="text-xl font-bold text-mainOrange-600 sm:text-4xl xl:text-6xl">
+        DIGIMON NOT FOUND !!!
+      </h1>
+    </div>
+  )
+}
