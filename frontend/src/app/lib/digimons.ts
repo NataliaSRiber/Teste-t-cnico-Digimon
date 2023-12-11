568096825c856f947ffb5d73805ddbdc8950eb19
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2023-12-11 20:34:06 -0300

    feat: add getDigimons function for API requests

---
+++ frontend/src/app/lib/digimons.ts
@@ -0,0 +1,17 @@
+import { api } from '../services/api'
+
+export async function getDigimons(context: any) {
+  const { search, setApiCall, setSearch, selectedLevel, setSelectedLevel } =
+    context
+
+  try {
+    const { data } = await api.get(`?name=${search}&level=${selectedLevel}`)
+    setApiCall(false)
+    setSelectedLevel('')
+    setSearch('')
+
+    return data
+  } catch (error) {
+    console.error(error)
+  }
+}
